98214a9cb6a5aaca2e127c1fb5d959dc1d605bc7
mrrobot911 <kelwin2705@gmail.com>
2024-04-22 23:11:01 +0300

    fix: remove controller

--- src/controllers/chat-controller.ts
+++ src/controllers/chat-controller.ts
@@ -1,11 +1,8 @@
 import BaseComponent from '../components/baseComponent';
 import ChatPage from '../pages/chat/chat';
-import WebSocketService from '../services/websoket-service';
 
 export default class ChatController {
   private readonly view: ChatPage;
-
-  private readonly webSocketService = new WebSocketService();
 
   constructor(private readonly root: BaseComponent<'section'>) {
     this.view = new ChatPage(this.root.getNode(), 'chat');
