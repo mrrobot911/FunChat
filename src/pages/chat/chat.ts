8168f232ce929385f9f39b12f7b7d5404121e2b9
mrrobot911 <kelwin2705@gmail.com>
2024-04-22 23:12:49 +0300

    fix: remove input

--- src/pages/chat/chat.ts
+++ src/pages/chat/chat.ts
@@ -1,12 +1,6 @@
 import BaseComponent from '../../components/baseComponent';
-import Input from '../../components/input/input';
 
 export default class ChatPage extends BaseComponent {
-  private readonly login: Input = new Input({
-    type: 'text',
-    placeholder: 'your login',
-  });
-
   private readonly usersList: BaseComponent = new BaseComponent({
     tag: 'div',
     className: 'users',
